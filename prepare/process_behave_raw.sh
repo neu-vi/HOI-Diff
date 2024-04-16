50f0946ecc010a43ccd3a7ab8466b4fbf14749ac
xiaogangpeng <1273483319@qq.com>
2024-04-16 02:32:15 -0400

    add raw_behave_process

---
+++ prepare/process_behave_raw.sh
@@ -0,0 +1,18 @@
+echo -e "spliting raw behave dataset"
+
+cd dataset
+
+mkdir behave-30fps-params
+
+tar -xvf behave-30fps-params-v1.tar -C ./behave-30fps-params/
+echo -e "Cleaning\n"
+rm behave-30fps-params-v1.tar
+
+echo -e "Cleaning Done!\n"
+
+echo -e "spliting data now!\n"
+
+cd ..
+python utils/behave_process.py
+
+echo -e "Done!"
